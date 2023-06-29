8dfe03f638d13e50a5141dc66e1f3d2e80af1cf5
Sheen <dan.80956@gmail.com>
2023-06-29 19:33:06 +0300

    completed hm4

--- src/channel.py
+++ src/channel.py
@@ -11,16 +11,14 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.__channel_id = channel_id
+        channel_info = self.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute()
 
-        self.youtube = build('youtube', 'v3', developerKey=API_KEY)
-        self.channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
-
-        self.title = self.channel['items'][0]['snippet']['title']
-        self.description = self.channel['items'][0]['snippet']['description']
+        self.title = channel_info['items'][0]['snippet']['title']
+        self.description = channel_info['items'][0]['snippet']['description']
         self.url = 'https://www.youtube.com/channel/' + self.__channel_id
-        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
-        self.video_count = self.channel['items'][0]['statistics']['videoCount']
-        self.view_count = self.channel['items'][0]['statistics']['viewCount']
+        self.subscriber_count = channel_info['items'][0]['statistics']['subscriberCount']
+        self.video_count = channel_info['items'][0]['statistics']['videoCount']
+        self.view_count = channel_info['items'][0]['statistics']['viewCount']
 
     def __str__(self):
         """ Возвращает название и ссылку на канал """
@@ -52,7 +50,9 @@ class Channel:
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+        youtube = build('youtube', 'v3', developerKey=API_KEY)
+        channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
 
     @property
     def channel_id(self):
@@ -62,7 +62,7 @@ class Channel:
     def get_service(cls):
         """ Возвращает объект для работы с YouTube API """
         youtube = build('youtube', 'v3', developerKey=API_KEY)
-        print(youtube)
+        return youtube
 
     def to_json(self, new_file):
         """ Сохраняет в файл значения атрибутов экземпляра Channel """
